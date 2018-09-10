468168980686c7b72cde62369546f7db28166e72
Tomer Gev <tomergev21@gmail.com>
2018-09-10 13:12:57 -0700

    Update init.js

--- test-browser/helpers/init.js
+++ test-browser/helpers/init.js
@@ -1,6 +1,6 @@
 module.exports = function (browser, callback) {
   browser
-    .url('http://127.0.0.1:8080/#version=builtin')
+    .url('http://172.16.4.5:8080/#version=builtin')
     .injectScript('test-browser/helpers/applytestmode.js', function () {
       browser.resizeWindow(2560, 1440, () => {
         browser.click('#autoCompile')
